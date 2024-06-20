97f73d1457796f9a0265b6e98d6cfc0d0fd4d30d
Tai Bui <tai.c.bui@gmail.com>
2024-06-21 01:41:54 +0700

    update

--- src/app/_component/count.tsx
+++ src/app/_component/count.tsx
@@ -5,7 +5,7 @@ import { useAppContext } from "../_context/context";
 export default function Home() {
   const {value} = useAppContext()
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       Number: {value}
     </main>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 import InputComponent from "./_component/InputComponent";
+import Count from "./_component/count";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24">
+    <main className="flex h-screen flex-col items-center gap-5 p-24">
+      <Count/>
       <InputComponent/>
-      <Link className="border rounded-lg border-green-400 p-3" href="/count">See result</Link>
     </main>
   );
 }
